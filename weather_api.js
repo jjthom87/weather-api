f77535ff71de26ee433a4983226c1c8b472e1652
Jared Thomas <jjthom87@yahoo.com>
2017-10-12 17:49:21 -0400

    latest updates

--- weather_api.js
+++ weather_api.js
@@ -3,6 +3,17 @@ $(document).ready(function(){
 	$('img').eq(0).css('height', '160px').css('margin-top', '25px');
 	$('img').eq(0).hide();
 
+
+	var weatherObj = [
+		{
+			main: "Clouds"
+		},
+		{
+
+		}
+	]
+
+
 	var str = "";
 	var city;
 	$('#city-input').on('keypress', function(e){
@@ -12,9 +23,10 @@ $(document).ready(function(){
 				$('img').eq(0).show();
 				city = $('#city-input').val().toLowerCase().split(" ").join("+");
 				$('#city-input').val("");
+				$('.weather-div').remove();
 				setTimeout(function(){
 					$('img').eq(0).hide();
-					var apiKey = '';
+					var apiKey = '275d5dfdea53a2d3e3869f48d154e9ac';
 					var weatherApiUrl = 'http://api.openweathermap.org/data/2.5/weather?appid=' + apiKey + '&units=imperial&q=';
 					weatherApiUrl += city;
 					$.ajax({
@@ -23,7 +35,12 @@ $(document).ready(function(){
 						success: function(response){
 							console.log(response)
 							var weatherDiv = $('<div>');
-							weatherDiv.addClass('weather-div');
+							weatherDiv.addClass('weather-div progress');
+							var gaugeDiv = $('<div id="gauge-div" role="progressbar" aria-valuenow="' + response.main.temp + '" aria-valuemin="0" aria-valuemax="100" style="width:' + response.main.temp + '%">');
+							gaugeDiv.addClass("progress-bar progress-bar-" + colorWeatherMatcher(response.main.temp));
+							gaugeDiv.text(Math.floor(response.main.temp) + "\xB0 F");
+							weatherDiv.append(gaugeDiv).append("<br>");
+							$(".weather-temp").append(weatherDiv)
 						}
 					});
 				}, 3000);
@@ -31,5 +48,15 @@ $(document).ready(function(){
 		}
 
 	});
+
+	function colorWeatherMatcher(temp){
+		if (temp < 50){
+			return "info";
+		} else if (temp < 75){
+			return "warning";
+		} else {
+			return "danger";
+		}
+	};
 
 });
